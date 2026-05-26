6f8431f90e2a4e085cc28580803f6716b75b4b68
DHARANEESHGEK-02 <dharaneeshgek1@gmail.com>
2026-05-26 14:21:12 +0530

    Switch from tensorflow.keras to keras for imports

--- LSTM/app.py
+++ LSTM/app.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
 
 st.set_page_config(page_title="Electricity Forecasting LSTM", layout="wide")
 
